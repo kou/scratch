fd9f270d19b3246802265ca90f9a7d46846e23ba
Kouhei Sutou <kou@cozmixng.org>
2003-12-02 16:37:47 +0000

    * Worked text client for number table.

--- sample/number-table/server.scm
+++ sample/number-table/server.scm
@@ -3,19 +3,18 @@
 (use dsm.server)
 (use number-table)
 
+(define (start-up)
+  (let ((table (shuffle-table (make-number-table 3))))
+    (lambda (command . args)
+      (case (string->symbol command)
+        ((show) table)
+        ((move) (move (make-keyword (x->string (car args)))
+                      table))
+        ((clear?) (clear? table))
+        (else "bad command")))))
+
 (define (main args)
   (let ((server (make-dsm-server :port 5969)))
-    (add-mount-point! server "start"
-                      (lambda (command . args)
-                        (let ((table (shuffle-table (make-number-table 3))))
-                          (define (move-table way)
-                            (move way table)
-                            move-table)
-                          
-                          (case command
-                            ((:show) table)
-                            ((:move) (move (car args) table))
-                            ((:clear?) (clear? table))
-                            (else "bad command")))))
+    (add-mount-point! server "start" start-up)
     (start-dsm-server server)))
                                            
