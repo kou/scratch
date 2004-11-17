be3200613c000842fb2a7638c8b99d4a2a04f240
Kouhei Sutou <kou@cozmixng.org>
2004-11-17 13:51:06 +0000

    * fixed paren corresponding bug.

--- lib/scratch/client/cgi.scm
+++ lib/scratch/client/cgi.scm
@@ -105,20 +105,20 @@
                (rxmatch-if (rxmatch #/\S+\;\s*charset=(\S+)/i content-type)
                    (charset)
                  charset
-                 output-encoding))))
-       (parameterize ((cgi-output-character-encoding output-encoding))
-         `(,(cond ((get-keyword :location header-info #f)
-                   => (cut make-cgi-header
-                           :cookies cookies
-                           :status "MOVED"
-                           :Location <>))
-                  (else
-                   (list
-                    (make-cgi-header :cookies cookies
-                                     :status status
-                                     :content-type content-type
-                                     :Content-Length (string-size body))
-                    body))))))
+                 output-encoding)))
+         (parameterize ((cgi-output-character-encoding output-encoding))
+           `(,(cond ((get-keyword :location header-info #f)
+                     => (cut make-cgi-header
+                             :cookies cookies
+                             :status "MOVED"
+                             :Location <>))
+                    (else
+                     (list
+                      (make-cgi-header :cookies cookies
+                                       :status status
+                                       :content-type content-type
+                                       :Content-Length (string-size body))
+                      body)))))))
      :merge-cookies #t
      :on-error error-proc)))
 
